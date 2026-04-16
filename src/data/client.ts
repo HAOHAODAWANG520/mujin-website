04d69da8de80cd627027cf7cf7f5e0d8a230d4b8
HW-MUJIN <harrywang.mujin@gmail.com>
2026-04-16 21:37:12 +0800

    Update client.ts

--- src/data/client.ts
+++ src/data/client.ts
@@ -11,21 +11,21 @@
  */
 
 export const client = {
-  name: 'Small Business Starter',
-  email: 'hello@example.com',
-  phoneForTel: '555-867-5309',
-  phoneFormatted: '(555) 867-5309',
+  name: 'MUJIN LIMITED',
+  email: 'harrywang.mujin@gmail.com',
+  phoneForTel: '+86 130-5165-3220',
+  phoneFormatted: '(0086) 130-5165-3220',
   /** Business / contractor license number. Displayed in the header and footer
    *  as a trust signal. Set to an empty string to hide it. */
   license: 'Lic# 123456',
   address: {
-    lineOne: '123 Main Street',
-    lineTwo: 'Suite 100',
-    city: 'Denver',
-    state: 'CO',
+    lineOne: '650 CHEUNG SHA WAN ROAD',
+    lineTwo: 'CHINA SHIPBUILDING TOWER 19F',
+    city: 'KOWLOON',
+    state: 'HONG KONG',
     zip: '80206',
-    country: 'US',
-    mapLink: 'https://maps.app.goo.gl/example',
+    country: 'CN',
+    mapLink: 'https://www.google.com/maps/place/China+Shipbuilding+Tower/@22.3387608,114.1522978,17z/data=!3m1!4b1!4m6!3m5!1s0x34040753cfb6fb2f:0xe5870f2cf4853f6d!8m2!3d22.3387608!4d114.1522978!16s%2Fg%2F11d_8cdjkb?entry=ttu&g_ep=EgoyMDI2MDQxMy4wIKXMDSoASAFQAw%3D%3D',
   },
   socials: {
     facebook: 'https://www.facebook.com/',
